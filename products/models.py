83608c31eb025a5c39af4418e11ea0897cfb3a90
YUBYUNGMOON <qudans1237@gmail.com>
2021-11-15 20:56:24 +0900

    modify: 리뷰 반영 수정 및 PaymentType 테이블 추가

---
+++ products/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Product(models.Model):
+    intro       = models.CharField(max_length=200)
+    title       = models.CharField(max_length=100)
+    price       = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.CharField(max_length=2000)
+    image_url   = models.CharField(max_length=2000)
+    deleted_at  = models.DateTimeField(null=True)
+
+    class Meta:
+        db_table = 'products'
+
+class Author(models.Model):
+    name         = models.CharField(max_length=30)
+    image_url    = models.CharField(max_length=2000)
+    introduction = models.CharField(max_length=2000)
+    product      = models.ForeignKey('Product',on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'authors'
