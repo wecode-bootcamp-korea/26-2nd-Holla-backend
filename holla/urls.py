673b23200f1724ee9b581eafbe683b4515781470
kimbongcheol <solarrrrr1010@gmail.com>
2021-11-23 16:33:50 +0900

    modify: 라이브 리뷰 반영, 리뷰 view 삭제

--- holla/urls.py
+++ holla/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 
+from products.views import MainTotalView
+
 urlpatterns = [
+    path('', MainTotalView.as_view()),
     path('products', include('products.urls')),
-    path('users', include('users.urls'))
+    path('users', include('users.urls')),
 ]
