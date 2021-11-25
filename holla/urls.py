e6e2e0c9c3b389a3af0ae8499cc706f3da499077
YUBYUNGMOON <qudans1237@gmail.com>
2021-11-25 19:03:02 +0900

    conflicts

--- holla/urls.py
+++ holla/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', MainTotalView.as_view()),
     path('products', include('products.urls')),
     path('users', include('users.urls')),
-    path('orders', include('orders.urls'))
+    path('orders', include('orders.urls')),
+    path('reviews'), include('reviews.urls')
 ]
