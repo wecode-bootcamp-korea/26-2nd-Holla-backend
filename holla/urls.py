180b433c110fb4a7c1175112dbf690d8ffe50db9
kimbongcheol <solarrrrr1010@gmail.com>
2021-11-25 17:44:43 +0900

    modify: 유닛테스트 및 리뷰 엔드포인트 수정

--- holla/urls.py
+++ holla/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', MainTotalView.as_view()),
     path('products', include('products.urls')),
     path('users', include('users.urls')),
+    path('reviews', include('reviews.urls'))
 ]
--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from products.views import ProductView
+from reviews.views  import ReviewView
 
 app_name = 'products'
 urlpatterns = [
     path('/<int:product_id>', ProductView.as_view()),
+    path('/<int:product_id>', ReviewView.as_view()),
 ]
