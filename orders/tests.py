4867188bd551ffbe74c7500b0c4aafa84745e92f
YUBYUNGMOON <qudans1237@gmail.com>
2021-11-25 13:06:45 +0900

    save

--- orders/tests.py
+++ orders/tests.py
@@ -1,5 +1,5 @@
 from django.http import response
-from django.test import TestCase, Client
+from django.test import TestCase, Client, client
 
 from orders.models   import ShoppingCart
 from products.models import Product, Author
@@ -8,6 +8,7 @@ from users.models    import User
 class CartGetTest(TestCase):
     def setup(self):
         self.client = Client()
+
         User.objects.create(
             id = 1,
             email = 'qudans@naver.com',    
@@ -15,91 +16,114 @@ class CartGetTest(TestCase):
             nickname = '김코드',
             kakao_id = 123456
         )
-        Product.objects.create(
+
+        Product.objects.bulk_create([
+            Product(
             id          = 1,
             intro       = "최고의 베스트셀러",
             title       = '100가지 장소의 보금자리',
             price       = "5800.00",
             description = "지금도 계속되고 있는 세계 각지로의 여행",
             image_url   = "https://wecode.com"
-        )
-        Product.objects.create(
-            id          = 2,
-            intro       = "최고의 베스트셀러",
-            title       = '100가지 장소의 보금자리',
-            price       = "5800.00",
-            description = "지금도 계속되고 있는 세계 각지로의 여행",
-            image_url   = "https://wecode.com"
-        )
-        Product.objects.create(
-            id          = 3,
-            intro       = "최고의 베스트셀러",
-            title       = '100가지 장소의 보금자리',
-            price       = "5800.00",
-            description = "지금도 계속되고 있는 세계 각지로의 여행",
-            image_url   = "https://wecode.com"
-        )
-        Product.objects.create(
-            id          = 4,
-            intro       = "최고의 베스트셀러",
-            title       = '100가지 장소의 보금자리',
-            price       = "5800.00",
-            description = "지금도 계속되고 있는 세계 각지로의 여행",
-            image_url   = "https://wecode.com"
-        )
-        Author.objects.create(
-            id           = 1,
-            name         = "이태훈",
-            image_url    = "https://wecode.com",
-            introduction = "최고의 베스트셀러 작가이다",
-            product_id   = 1
-        )
-        Author.objects.create(
-            id           = 2,
-            name         = "김태훈",
-            image_url    = "https://wecode.com",
-            introduction = "최고의 베스트셀러 작가이다",
-            product_id   = 2
-        )
-        Author.objects.create(
-            id           = 3,
-            name         = "삼태훈",
-            image_url    = "https://wecode.com",
-            introduction = "최고의 베스트셀러 작가이다",
-            product_id   = 3
-        )
-        Author.objects.create(
-            id           = 4,
-            name         = "사태훈",
-            image_url    = "https://wecode.com",
-            introduction = "최고의 베스트셀러 작가이다",
-            product_id   = 4
-        )
+            ),
+            Product(
+                id          = 2,
+                intro       = "최고의 베스트셀러",
+                title       = '100가지 장소의 보금자리',
+                price       = "5800.00",
+                description = "지금도 계속되고 있는 세계 각지로의 여행",
+                image_url   = "https://wecode.com"
+            ),
+            Product(
+                id          = 3,
+                intro       = "최고의 베스트셀러",
+                title       = '100가지 장소의 보금자리',
+                price       = "5800.00",
+                description = "지금도 계속되고 있는 세계 각지로의 여행",
+                image_url   = "https://wecode.com" 
+            ),
+            Product(
+                id          = 4,
+                intro       = "최고의 베스트셀러",
+                title       = '100가지 장소의 보금자리',
+                price       = "5800.00",
+                description = "지금도 계속되고 있는 세계 각지로의 여행",
+                image_url   = "https://wecode.com"    
+            ),
+            Product(
+                id          = 5,
+                intro       = "최고의 베스트셀러",
+                title       = '100가지 장소의 보금자리',
+                price       = "5800.00",
+                description = "지금도 계속되고 있는 세계 각지로의 여행",
+                image_url   = "https5://wecode.com"    
+            )
+        ])
 
-        ShoppingCart.objects.create(
-            user_id = 1,
-            product_id =1,
-            order_status_id =1,
-            payment_type_id =1,
-        )
-        ShoppingCart.objects.create(
-            user_id = 1,
-            product_id =2,
-            order_status_id =1,
-            payment_type_id =1,
-        )
-        ShoppingCart.objects.create(
-            user_id = 1,
-            product_id =3,
-            order_status_id =1,
-            payment_type_id =1,
-        )
-        ShoppingCart.objects.create(
-            user_id = 1,
-            product_id =4,
-            order_status_id =1,
-            payment_type_id =1,
-        )
+        Author.objects.bulk_create([
+            Author(
+                id           = 1,
+                name         = "이태훈",
+                image_url    = "https://wecode.com",
+                introduction = "최고의 베스트셀러 작가이다",
+                product_id   = 1
+            ),
+            Author(
+                id           = 2,
+                name         = "김태훈",
+                image_url    = "https://wecode.com",
+                introduction = "최고의 베스트셀러 작가이다",
+                product_id   = 2
+            ),
+            Author(
+                id           = 3,
+                name         = "삼태훈",
+                image_url    = "https://wecode.com",
+                introduction = "최고의 베스트셀러 작가이다",
+                product_id   = 3
+            ),
+            Author(
+                id           = 4,
+                name         = "사태훈",
+                image_url    = "https://wecode.com",
+                introduction = "최고의 베스트셀러 작가이다",
+                product_id   = 4
+            ),
+            Author(
+                id           = 5,
+                name         = "오태훈",
+                image_url    = "https://wecode.com",
+                introduction = "최고의 베스트셀러 작가이다",
+                product_id   = 5
+            )
+        ])
+
+        ShoppingCart.objects.bulk_create([
+            ShoppingCart(
+                user_id = 1,
+                roduct_id =1,
+                order_status_id =1,
+                payment_type_id =1,
+            ),
+            ShoppingCart(
+                user_id = 1,
+                product_id =2,
+                order_status_id =1,
+                payment_type_id =1,
+            ),
+            ShoppingCart(
+                user_id = 1,
+                product_id =3,
+                order_status_id =1,
+                payment_type_id =1,
+            ),
+            ShoppingCart(
+                user_id = 1,
+                product_id =4,
+                order_status_id =1,
+                payment_type_id =1,
+            )
+        ])
 
 def tearDown(self):
         User.objects.all().delete()
@@ -108,47 +132,68 @@ def tearDown(self):
         ShoppingCart.objects.all().delete()
 
 def test_get_cart_succes(self):
-    response = self.client.get('/orders/crat')
+    response = self.client.get('/orders/cart')
 
     self.assertEqual(response.json(),({
         'cart_items': [
             {
-                'product_id': 1,
-                'user_id': 1,
-                'product_list': {
-                'product_title': '100가지 장소의 보금자리',
-                'product_image_url': "https://wecode.com",
-                'product_price': 5800,
-                'author_name': '이태훈'
-                }},
+                'product_id'        : 1,
+                'user_id'           : 1,
+                'product_list'      : {
+                    'product_title'     : '100가지 장소의 보금자리',
+                    'product_image_url' : "https://wecode.com",
+                    'product_price'     : 5800,
+                    'author_name'       : '이태훈'
+                }
+            },
             {
-                'product_id': 2,
-                'user_id': 1,
-                'product_list': {
-                'product_title': '100가지 장소의 보금자리',
-                'product_image_url': "https://wecode.com",
-                'product_price': 5800,
-                 'author_name': '김태훈'
-                 }},
+                'product_id'        : 2,
+                'user_id'           : 1,
+                'product_list'      : {
+                    'product_title'     : '100가지 장소의 보금자리',
+                    'product_image_url' : "https://wecode.com",
+                    'product_price'     : 5800,
+                    'author_name'       : '김태훈'
+                 }
+            },
             {
-                'product_id': 3,
-                'user_id': 1,
-                'product_list': {
-                'product_title': '100가지 장소의 보금자리',
-                'product_image_url': "https://wecode.com",
-                'product_price': 5800,
-                 'author_name': '삼태훈'
-                 }},
+                'product_id'        : 3,
+                'user_id'           : 1,
+                'product_list'      : {
+                    'product_title'     : '100가지 장소의 보금자리',
+                    'product_image_url' : "https://wecode.com",
+                    'product_price'     : 5800,
+                    'author_name'       : '삼태훈'
+                 }
+            },
             {
-                'product_id': 4,
-                'user_id': 1,
-                'product_list': {
-                'product_title': '100가지 장소의 보금자리',
-                 'product_image_url': "https://wecode.com",
-                  'product_price': 5800,
-                   'author_name': '사태훈'
-                   }}], 
+                'product_id'        : 4,
+                'user_id'           : 1,
+                'product_list'      : {
+                    'product_title'     : '100가지 장소의 보금자리',
+                    'product_image_url' : "https://wecode.com",
+                    'product_price'     : 5800,
+                    'author_name'       : '사태훈'
+                   }
+            }], 
         'total_product_price': 23200,
         'total_product': 4
-        }))
+        }
+        ))
+
     self.assertEqual(response.status_code, 200)
+
+def test_post_cart_succes(self):
+    shoppingCart = {
+        'user_id'    : 1,
+        'product_id' : 5
+    }
+
+    response = self.client.post('/orders/cart', json.dumps(shoppingCart), content_type='application/json')
+
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.json(),
+        {
+            'message' : 'SUCCESS'
+        }
+    )
--- orders/views.py
+++ orders/views.py
@@ -2,7 +2,7 @@ import json,requests
 
 from django.views     import View
 from django.http      import JsonResponse
-from django.db.models import Q
+from django.db.models import Sum
 
 from orders.models   import ShoppingCart
 from products.models import Author
@@ -12,22 +12,27 @@ class CartGetView(View):
     @login_decorator
     def get(self, request):
         try:
-            carts= ShoppingCart.objects.filter(user_id = request.user.id)
+            carts = ShoppingCart.objects.filter(user_id = request.user.id)
             cart_items = [{
-                "product_id"        : shoppingcart.product.id,
-                "user_id"           : shoppingcart.user.id,
-                "product_list"      :{
-                "product_title"     : shoppingcart.product.title,
-                "product_image_url" : shoppingcart.product.image_url,
-                "product_price"     : int(shoppingcart.product.price),
-                "author_name"       : Author.objects.get(product_id=shoppingcart.product.id).name,
+                "product_id"   : shoppingcart.product.id,
+                "user_id"      : shoppingcart.user.id,
+                "product_list" : {
+                    "product_title"     : shoppingcart.product.title,
+                    "product_image_url" : shoppingcart.product.image_url,
+                    "product_price"     : int(shoppingcart.product.price),
+                    "author_name"       : Author.objects.get(product_id=shoppingcart.product.id).name,
                 }
-            }for shoppingcart in carts]
+            } for shoppingcart in carts]
 
-            total_product       = len([(shoppingcart.product.id) for shoppingcart in carts])
-            total_product_price = sum([int(shoppingcart.product.price) for shoppingcart in carts])
+            total_product_price = carts.aggregate(Sum('product__price'))
             
-            return JsonResponse({"cart_items" : cart_items, "total_product_price" :total_product_price, "total_product":total_product}, status=200)
+            result = {
+            "cart_items"          : cart_items,
+            "total_product_price" : total_product_price,
+            "num_product"         : len(carts)
+            }
+
+            return JsonResponse(result, status=200)
 
         except ShoppingCart.DoesNotExist:
             return JsonResponse({"message" : "SHOPPINGCART_DOES_NOT_EXIST"}, status = 400)
@@ -36,13 +41,12 @@ class CartPostView(View):
     @login_decorator
     def post(self, request):
         try:
-            data              = json.loads(request.body)
-            product_id        = data['product_id']
-            user_id           = data['user_id']
+            data       = json.loads(request.body)
+            product_id = data['product_id']
 
             ShoppingCart.objects.create(
-                product_id        = product_id,
-                user_id           = user_id,
+                product_id = product_id,
+                user_id    = request.user.id,
             )
             return JsonResponse({'message' : 'SUCCESS'}, status=201)
 
@@ -51,7 +55,7 @@ class CartPostView(View):
 
 class CartDeleteView(View):
     login_decorator
-    def delete(self, request, id):
-        ShoppingCart.objects.filter(Q(user_id = request.user.id) & Q(product_id = id)).delete()
+    def delete(self, request, product_id):
+        ShoppingCart.objects.filter(user_id = request.user.id, product_id = product_id).delete()
 
         return JsonResponse({"message" : "SUCCESS"}, status=204)
